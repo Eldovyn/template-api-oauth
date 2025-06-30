934b99a8b7fcd5f568a4d68c8bb862d5e080027d
Eldovyn <farras.pramudita@gmail.com>
2025-06-30 12:43:24 +0700

    refactor: refactor "app factory"

--- app/controllers/register.py
+++ app/controllers/register.py
@@ -5,7 +5,7 @@ import requests
 import re
 from ..utils import TokenEmailAccountActive, TokenWebAccountActive, SendEmail, AuthJwt
 import datetime
-from ..config import provider as PROVIDER
+from ..configs import provider as PROVIDER
 import random
 import string
 from ..serializers import UserSerializer, TokenSerializer
@@ -20,7 +20,7 @@ class RegisterController:
     async def user_register(
         self, provider, token, username, email, password, confirm_password, timestamp
     ):
-        from ..bcrypt import bcrypt
+        from ..extensions import bcrypt
 
         access_token = None
         token_web = None
---
+++ app/middlewares.py
@@ -0,0 +1,17 @@
+import datetime
+from flask import request
+
+
+def register_middlewares(app):
+    @app.after_request
+    async def add_cors_headers(response):
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = (
+            "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+        )
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
+    @app.before_request
+    async def before_request():
+        request.timestamp = datetime.datetime.now(datetime.timezone.utc)
